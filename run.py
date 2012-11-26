709fc5351441406f8cd639cb3f51cb2e72d0acf5
Kurt <kespindler@gmail.com>
2012-11-26 01:15:19 -0500

    initial commit

---
+++ run.py
@@ -0,0 +1,62 @@
+from pylov import run_experiment
+from pylov.pygamerenderer import PygameRenderer
+from pylov.lightroom import Lightroom
+from pylov.agent import RandomAgent
+from numpy import array
+from lightroom import field
+
+#0, ' ' is empty
+#1 is wall
+#2 is player
+#3 is key
+#4 is lock
+#5 is door
+lightroom_string = """\
+1111111111111
+12   111   11
+1    5   3 11
+1    111   11
+13   111   11
+1    41151411
+1    111   11
+11111111   61
+11111111   11
+1111111115111
+1111111111111"""
+
+def fill_grid(self):
+    for i in range(self.maze.states.shape[0]):
+        for j in range(self.maze.states.shape[1]):
+            if self.maze.states[i, j] == field.WALL:
+                color = GREEN
+                self.screen.fill(GREEN, self.make_rect(i,j))
+            else:
+                self.screen.fill(LIGHTGREY, self.make_rect(i,j))
+                if self.maze.states[i,j] == field.KEY:
+                    self.screen.blit(self.key_img, img_point(i,j))
+                elif self.maze.states[i,j] == field.KEY_LOCK:
+                    self.screen.blit(self.lock_img, img_point(i,j))
+                    self.screen.blit(self.key_img, img_point(i,j))
+                elif self.maze.states[i,j] == field.LOCK_DOOR:
+                    self.screen.blit(self.door_img, img_point(i,j))
+                elif self.maze.states[i,j] == field.KEY_LOCK:
+                    self.screen.blit(self.lock_img, img_point(i,j))
+    self.screen.blit(self.goal_img, img_point(*self.maze.goal))
+
+def arr_from_str(string):
+    string = string.replace(' ', '0')
+    lines = string.split('\n')
+    return array([[int(z) for z in l] for l in lines])
+
+structure = arr_from_str(lightroom_string)
+print structure
+lock_pairs = {}
+lock_pairs[5,5] = (5, 2)
+lock_pairs[10,5] = (8, 5)
+lock_pairs[11,7] = (9, 9)
+env = Lightroom(structure, (9, 9), lock_pairs)
+agent = RandomAgent()
+rend = PygameRenderer(env)
+rend.fill_grid = fill_grid
+
+run_experiment(env, agent, rend)
