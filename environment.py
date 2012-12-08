9ac9c2bae6abd8ee0a2b78b2a13a82cb2f4add6b
Kurt <kespindler@gmail.com>
2012-12-08 18:03:34 -0500

    fix bug where environment wasn't reset between iterations

--- environment.py
+++ environment.py
@@ -101,6 +101,8 @@ class Lightworld(Environment):
                 if not self.episode_finished(state):
                     self.set_room(self.room + 1)
                     reward = self.task_reward
+                    state2 = self.new_state()
+                    pos2 = (state2.x, state2.y)
                 else:
                     reward = self.final_reward
         elif action == a.G:
