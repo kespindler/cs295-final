fdd63f825b76f6fed554ededa05706d2db4309ed
rdlester <sursiks.010@gmail.com>
2012-12-09 23:48:41 -0500

    RLSSarsaAgent in Run

--- run.py
+++ run.py
@@ -3,18 +3,20 @@ from environment import Lightworld
 from lightworld_gen import gen_world
 from agent import SarsaAgent, RandomAgent
 from options import OptionAgent
+from mlagent import RLSSarsaAgent
 from pygamerenderer import PygameRenderer
 from utility import rooms_from_fpath, run_experiment
 
 problemSpaceDim = 5
 agentSpaceDim = 12
+actionDesc = (6,)
 
 rooms = gen_world()
 env = Lightworld(*[room for iskey, room in rooms])
 env = Lightworld(*rooms_from_fpath('basic_lightworld.txt'))
-stateDesc = env.dimensions()[0:problemSpaceDim]
-actionDesc = (6,)
-agent = OptionAgent(stateDesc, actionDesc)
+# stateDesc = env.dimensions()[0:problemSpaceDim]
+# agent = OptionAgent(stateDesc, actionDesc)
+agent = RLSSarsaAgent(agentSpaceDim, problemSpaceDim, actionDesc[0])
 rend = PygameRenderer(env)
 
 run_experiment(env, agent, rend)
