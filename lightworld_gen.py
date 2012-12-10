8359ff766e2bf58b9702b5eb574c110bb36ab902
Kurt <kespindler@gmail.com>
2012-12-10 00:29:36 -0500

    output lightworlds to text files

--- lightworld_gen.py
+++ lightworld_gen.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #from lightroom import Lightroom
 #from lightroom_gen import make_rand_room
 from lightroom_struct import LightroomStruct as LS
@@ -54,39 +55,51 @@ def gen_100():
     return list_of_worlds
             
             
-gen_100()    
-   
-#gen_world()
+#gen_100()    
+#   
+##gen_world()
+#
+#room_arr1 = numpy.zeros((3, 3), dtype=int)
+#room_arr2 = numpy.zeros((3, 3), dtype=int)
+#room_arr3 = numpy.zeros((3, 3), dtype=int)
+#room_arr3[0, :] = 1
+#room_arr3[1, :] = 2
+##print("room_arr1:")
+##print(room_arr1)
+##print("room_arr2:")
+##print(room_arr2)
+##print("room_arr3:")
+##print(room_arr3)
+#lr11 = LS(1, room_arr1).return_tuple()
+#lr12 = LS(1, room_arr2).return_tuple()
+#lr13 = LS(1, room_arr3).return_tuple()
+#lr01 = LS(0, room_arr1).return_tuple()
+#
+#lw1 = [lr11, lr13]
+#lw2 = [lr12, lr13]
+#lw3 = [lr01, lr13]
+#lw4 = [lr11, lr13, lr12]
+#
+#assert (lightroom_equal(lr11, lr12))
+#assert (lightroom_equal(lr13, lr13))
+#assert (not lightroom_equal(lr11, lr13))
+#assert (not lightroom_equal(lr11, lr01))
+#assert (not lightroom_equal(lr13, lr01))
+#
+#assert (lightworld_equal(lw1, lw2))
+#assert (not lightworld_equal(lw1, lw3))
+#assert (not lightworld_equal(lw1, lw4))
 
-room_arr1 = numpy.zeros((3, 3), dtype=int)
-room_arr2 = numpy.zeros((3, 3), dtype=int)
-room_arr3 = numpy.zeros((3, 3), dtype=int)
-room_arr3[0, :] = 1
-room_arr3[1, :] = 2
-#print("room_arr1:")
-#print(room_arr1)
-#print("room_arr2:")
-#print(room_arr2)
-#print("room_arr3:")
-#print(room_arr3)
-lr11 = LS(1, room_arr1).return_tuple()
-lr12 = LS(1, room_arr2).return_tuple()
-lr13 = LS(1, room_arr3).return_tuple()
-lr01 = LS(0, room_arr1).return_tuple()
-
-lw1 = [lr11, lr13]
-lw2 = [lr12, lr13]
-lw3 = [lr01, lr13]
-lw4 = [lr11, lr13, lr12]
-
-assert (lightroom_equal(lr11, lr12))
-assert (lightroom_equal(lr13, lr13))
-assert (not lightroom_equal(lr11, lr13))
-assert (not lightroom_equal(lr11, lr01))
-assert (not lightroom_equal(lr13, lr01))
-
-assert (lightworld_equal(lw1, lw2))
-assert (not lightworld_equal(lw1, lw3))
-assert (not lightworld_equal(lw1, lw4))
-
-
+if __name__ == '__main__':
+    worlds = gen_100()
+    import os
+    folder = 'lightworlds'
+    try:
+        os.mkdir(folder)
+    except OSError:
+        pass
+    for i, world in enumerate(worlds):
+        world = [w for k, w in world]
+        roomstrings = ['\n'.join(''.join(str(i) for i in row) for row in room) for room in world]
+        with open(os.path.join(folder, '{0:03}.txt'.format(i)), 'w') as f:
+            f.write('\n\n'.join(roomstrings))
