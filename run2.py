1aac4ad4bc13db62e6f5dcf94a3ff2e833ff0d76
Kurt <kespindler@gmail.com>
2012-12-11 11:43:06 -0500

    push up the profiler run

---
+++ run2.py
@@ -0,0 +1,62 @@
+import numpy as np
+from environment import Lightworld
+from lightworld_gen import gen_world
+from agent import SarsaAgent, RandomAgent
+from options import OptionAgent
+try:
+    from pygamerenderer import PygameRenderer
+except:
+    pass
+from utility import rooms_from_fpath, run_experiment
+import os
+import matplotlib.pyplot as plt
+import multiprocessing as mp
+import cProfile
+
+problemSpaceDim = 5
+#agentSpaceDim = 12
+actionDesc = (6,)
+
+folder = 'lightworlds'
+worldfnames = [os.path.join(folder, f) for f in os.listdir(folder)]
+
+N_DUPS = 1
+#N_DUPS = 10 #10
+N_EPISODES = 1
+#N_EPISODES = 1
+worldfnames = worldfnames[:1] # dont' do this
+#q = mp.Queue()
+
+def run_iteration(lightworldfname):
+    print 'Begin lightworld', lightworldfname
+    env = Lightworld(*rooms_from_fpath(lightworldfname))
+    stateDesc = env.dimensions()[0:problemSpaceDim]
+    actionDesc = (6,)
+    agent = SarsaAgent(stateDesc, actionDesc)
+    #rend = PygameRenderer(env)
+    rend = None
+    episode_lengths = run_experiment(env, agent, rend, episodes = N_EPISODES)
+    #q.put(episode_lengths)
+
+
+def run_list():
+    for f in worldfnames:
+        run_iteration(f)
+
+#run_list()
+cProfile.run('run_iteration(worldfnames[0])', 'run.profile')
+#run_iteration(worldfnames[0])
+#lightworldfpaths = [f for f in worldfnames for _ in range(N_DUPS)]
+#pool = mp.Pool(8)
+#pool.map(run_iteration, lightworldfpaths)
+
+#resultarr = np.zeros((N_DUPS * len(worldfnames),N_EPISODES))
+#i = 0
+#while not q.empty():
+#    resultarr[i,:] = q.get()
+#    i += 1
+
+#means = resultarr.mean(0)
+#np.savetxt('sarsaagent.csv', resultarr,delimiter=',')
+#plt.plot(means)
+#plt.savefig('sarsaagent.png')
